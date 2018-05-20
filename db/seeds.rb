e4cb29466556991274dfaa8378410bef9f1f0f3b
sla10132000@yahoo.co.jp <sla10132000@yahoo.co.jp>
2018-05-20 12:47:52 +0900

    modified commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #5.times do |no|
-    Sale.create(:uriage_gaku => "100000000" ,:uriage_date => "20180501")
-    Sale.create(:uriage_gaku => "300000000" ,:uriage_date => "20180601")
-    Sale.create(:uriage_gaku => "50000000" ,:uriage_date => "20180701")
-    Sale.create(:uriage_gaku => "88000000" ,:uriage_date => "20180801")
+Sale.create(:uriage_gaku => "50000000" ,:uriage_date => "20180201")
+Sale.create(:uriage_gaku => "60000000" ,:uriage_date => "20180301")
+Sale.create(:uriage_gaku => "70000000" ,:uriage_date => "20180401")
+Sale.create(:uriage_gaku => "80000000" ,:uriage_date => "20180501")
+Sale.create(:uriage_gaku => "90000000" ,:uriage_date => "20180601")
+Sale.create(:uriage_gaku => "100000000" ,:uriage_date => "20180701")
+Sale.create(:uriage_gaku => "118000000" ,:uriage_date => "20180801")
 #  end
